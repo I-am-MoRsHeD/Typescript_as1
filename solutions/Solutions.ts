067cdae8086420f057fb536112a0b3b05e05a9d1
MoRsHeD <mdmorshed0187@gmail.com>
2025-05-07 23:56:04 +0600

    solved problem 2

--- solutions/Solutions.ts
+++ solutions/Solutions.ts
@@ -11,3 +11,27 @@ const formString = (param: string, toUpper?: boolean): string => {
 
 const r1 = formString('HELLO');
 // console.log(r1);
+
+
+// problem 2
+interface Book {
+    title: string;
+    rating: number;
+};
+
+const filterByRating = (books: Array<Book>) => {
+    if (!books) {
+        return;
+    } else {
+        return books.filter(book => book.rating > 4);
+    }
+};
+
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+
+const r2 = filterByRating(books);
+// console.log(r2);
